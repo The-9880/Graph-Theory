2760c8f9514aa55b39fe132c91bc4198a2047a02
Anant Sharma <anant98@hotmail.com>
2018-05-17 15:38:14 -0400

    Today's a slow, slow, sloooow day.
    I'm trying a recursive definition to return a single path from BFS to the value searched for, if it can be found in the graph.
    
    The function is bad right now... I'm tired. I'll fix it later/tomorrow.

--- BFS Maze/GraphAlgs.h
+++ BFS Maze/GraphAlgs.h
@@ -60,6 +60,48 @@ public:
 		unmarkNodes(allVisited);
 	}
 
+	void pathTo(Vertex<T>* v, T target)
+	{
+		std::queue<Vertex<T>*> verts;
+		std::list<Vertex<T>*> allVisited;
+
+		verts.push_back(&v);
+
+		recursiveBFSPath(verts, target, allVisited);	//	Modify recursiveBFSPath to function under this definition.
+	}
+
+	bool recursiveBFSPath(std::queue<Vertex<T>*>& v, T target, std::list<Vertex<T>*>& vertices, bool flag=true)
+	{
+		std::list<Vertex<T>&> unVis;
+
+		//Base case: found the target.
+		if (v.visit() == target)
+		{
+			vertices.push_back(&v);
+			return true;
+		}
+		else
+		{
+			//	2 more cases to cover: 1) This isn't the target, and there are no more vertices to visit -- failed to find target value.
+			//	Other case: There are still more vertices to visit.
+			unVis = v.getUnvisitedVertices();
+			if (unVis.empty())
+			{
+				//	Case one:
+				return false;
+			}
+			else
+			{
+				for (auto& vertex : unVis)
+				{
+					vertices.push_back(vertex);
+				}
+				return recursiveBFSPath(v, target, vertices, true);
+			}
+		}
+		
+	}
+
 private:
 
 	//	Used to unmark nodes in a list of pointers to vertices; not in use yet because I'd like to retain the testing code I have.
